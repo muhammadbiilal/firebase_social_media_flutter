6c0421e6e4edf6aa9e6bea643e2106ac8fa57aa9
muhammadbiilal <cs21.bilal@gmail.com>
2024-11-10 19:19:27 +0500

    app theme design

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:tech_media/res/color.dart';
+import 'package:tech_media/res/fonts.dart';
 import 'package:tech_media/utils/routes/route_name.dart';
 import 'package:tech_media/utils/routes/routes.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
-
 
 import 'firebase_options.dart';
 
@@ -30,7 +31,80 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: AppColors.primaryMaterialColor,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          color: AppColors.whiteColor,
+          centerTitle: true,
+          titleTextStyle: TextStyle(
+            fontSize: 12,
+            fontFamily: AppFonts.sfProDisplayMedium,
+            color: AppColors.primaryTextTextColor,
+          ),
+        ),
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            fontSize: 40,
+            fontFamily: AppFonts.sfProDisplayMedium,
+            color: AppColors.primaryTextTextColor,
+            fontWeight: FontWeight.w500,
+            height: 1.6,
+          ),
+          displayMedium: TextStyle(
+            fontSize: 32,
+            fontFamily: AppFonts.sfProDisplayMedium,
+            color: AppColors.primaryTextTextColor,
+            fontWeight: FontWeight.w500,
+            height: 1.6,
+          ),
+          displaySmall: TextStyle(
+            fontSize: 28,
+            fontFamily: AppFonts.sfProDisplayMedium,
+            color: AppColors.primaryTextTextColor,
+            fontWeight: FontWeight.w500,
+            height: 1.9,
+          ),
+          headlineMedium: TextStyle(
+            fontSize: 24,
+            fontFamily: AppFonts.sfProDisplayMedium,
+            color: AppColors.primaryTextTextColor,
+            fontWeight: FontWeight.w500,
+            height: 1.6,
+          ),
+          headlineSmall: TextStyle(
+            fontSize: 20,
+            fontFamily: AppFonts.sfProDisplayMedium,
+            color: AppColors.primaryTextTextColor,
+            fontWeight: FontWeight.w500,
+            height: 1.6,
+          ),
+          titleLarge: TextStyle(
+            fontSize: 17,
+            fontFamily: AppFonts.sfProDisplayBold,
+            color: AppColors.primaryTextTextColor,
+            fontWeight: FontWeight.w700,
+            height: 1.6,
+          ),
+          bodyLarge: TextStyle(
+            fontSize: 17,
+            fontFamily: AppFonts.sfProDisplayBold,
+            color: AppColors.primaryTextTextColor,
+            fontWeight: FontWeight.w700,
+            height: 1.6,
+          ),
+          bodyMedium: TextStyle(
+            fontSize: 14,
+            fontFamily: AppFonts.sfProDisplayRegular,
+            color: AppColors.primaryTextTextColor,
+            height: 1.6,
+          ),
+          bodySmall: TextStyle(
+            fontSize: 12,
+            fontFamily: AppFonts.sfProDisplayBold,
+            color: AppColors.primaryTextTextColor,
+            height: 2.26,
+          ),
+        ),
       ),
       // home: const SplashScreen(),
       initialRoute: RouteName.splashScreen,
@@ -38,3 +112,15 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// Here’s how the old properties map to the new ones:
+//
+// headline1 → displayLarge
+// headline2 → displayMedium
+// headline3 → displaySmall
+// headline4 → headlineMedium
+// headline5 → headlineSmall
+// headline6 → titleLarge
+// bodyText1 → bodyLarge
+// bodyText2 → bodyMedium
+// caption → bodySmall
