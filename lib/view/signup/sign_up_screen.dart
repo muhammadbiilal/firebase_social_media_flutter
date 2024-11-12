1c3fbb798c033cc5f821bf1514821995e985dc2b
Muhammad Bilal <cs21.bilal@gmail.com>
2024-11-12 18:49:26 +0500

    sign up firabase

--- lib/view/signup/sign_up_screen.dart
+++ lib/view/signup/sign_up_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:tech_media/res/component/input_text_field.dart';
 import 'package:tech_media/res/component/round_button.dart';
 import 'package:tech_media/utils/routes/route_name.dart';
+import 'package:tech_media/utils/utils.dart';
+import 'package:tech_media/view_model/signup/signup_controller.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -40,108 +43,130 @@ class _SignUpScreenState extends State<SignUpScreen> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(height: height * 0.01),
-                Text(
-                  'Welcome to App',
-                  style: Theme.of(context).textTheme.displaySmall,
-                ),
-                SizedBox(height: height * 0.01),
-                Text(
-                  'Enter you email address\nto register your account',
-                  style: Theme.of(context).textTheme.titleMedium,
-                  textAlign: TextAlign.center,
-                ),
-                Form(
-                    key: _formKey,
-                    child: Padding(
-                      padding: EdgeInsets.only(
-                          top: height * 0.06, bottom: height * 0.01),
-                      child: Column(
-                        children: [
-                          // SizedBox(height: height * 0.01),
-                          InputTextField(
-                            myController: emailController,
-                            focusNode: emailFocusNode,
-                            onFieldSubmittedValue: (value) {},
-                            keyboardType: TextInputType.emailAddress,
-                            obscureText: false,
-                            hint: 'Email',
-                            onValidator: (value) {
-                              return value.isEmpty ? "enter email" : null;
-                            },
-                          ),
-                          SizedBox(height: height * 0.01),
-                          InputTextField(
-                            myController: passwordController,
-                            focusNode: passwordFocusNode,
-                            onFieldSubmittedValue: (value) {},
-                            keyboardType: TextInputType.emailAddress,
-                            obscureText: false,
-                            hint: 'Password',
-                            onValidator: (value) {
-                              return value.isEmpty ? "enter password" : null;
-                            },
-                          ),
-                          SizedBox(height: height * 0.01),
-
-                          InputTextField(
-                            myController: userNameController,
-                            focusNode: userNameFocusNode,
-                            onFieldSubmittedValue: (value) {},
-                            keyboardType: TextInputType.emailAddress,
-                            obscureText: false,
-                            hint: 'Username',
-                            onValidator: (value) {
-                              return value.isEmpty ? "enter username" : null;
-                            },
-                          ),
-                        ],
+          child: ChangeNotifierProvider(
+            create: (context) => SignupController(),
+            child: Consumer<SignupController>(
+              builder: (context, provider, child) {
+                return SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(height: height * 0.01),
+                      Text(
+                        'Welcome to App',
+                        style: Theme.of(context).textTheme.displaySmall,
                       ),
-                    )),
-                const SizedBox(height: 40),
-                RoundButton(
-                  title: 'Sign Up',
-                  onPress: () {
-                    if (_formKey.currentState!.validate()) {
-                      // TODO: Implement login logic
-                      emailController.clear();
-                      passwordController.clear();
-                      emailFocusNode.unfocus();
-                      passwordFocusNode.unfocus();
-                    }
-                  },
-                ),
-                SizedBox(height: height * 0.03),
-                InkWell(
-                  onTap: () {
-                    Navigator.pushNamed(context, RouteName.loginView);
-                  },
-                  child: Text.rich(
-                    TextSpan(
-                        text: "Already have an account? ",
-                        style: Theme.of(context)
-                            .textTheme
-                            .titleMedium!
-                            .copyWith(fontSize: 15),
-                        children: [
+                      SizedBox(height: height * 0.01),
+                      Text(
+                        'Enter you email address\nto register your account',
+                        style: Theme.of(context).textTheme.titleMedium,
+                        textAlign: TextAlign.center,
+                      ),
+                      Form(
+                          key: _formKey,
+                          child: Padding(
+                            padding: EdgeInsets.only(
+                                top: height * 0.06, bottom: height * 0.01),
+                            child: Column(
+                              children: [
+                                // SizedBox(height: height * 0.01),
+                                InputTextField(
+                                  myController: userNameController,
+                                  focusNode: userNameFocusNode,
+                                  onFieldSubmittedValue: (value) {},
+                                  keyboardType: TextInputType.emailAddress,
+                                  obscureText: false,
+                                  hint: 'Username',
+                                  onValidator: (value) {
+                                    return value.isEmpty
+                                        ? "enter username"
+                                        : null;
+                                  },
+                                ),
+                                SizedBox(height: height * 0.01),
+                                InputTextField(
+                                  myController: emailController,
+                                  focusNode: emailFocusNode,
+                                  onFieldSubmittedValue: (value) {
+                                    Utils.fieldFocus(context, emailFocusNode,
+                                        passwordFocusNode);
+                                  },
+                                  keyboardType: TextInputType.emailAddress,
+                                  obscureText: false,
+                                  hint: 'Email',
+                                  onValidator: (value) {
+                                    return value.isEmpty ? "enter email" : null;
+                                  },
+                                ),
+                                SizedBox(height: height * 0.01),
+                                InputTextField(
+                                  myController: passwordController,
+                                  focusNode: passwordFocusNode,
+                                  onFieldSubmittedValue: (value) {
+                                    Utils.fieldFocus(context, passwordFocusNode,
+                                        userNameFocusNode);
+                                  },
+                                  keyboardType: TextInputType.emailAddress,
+                                  obscureText: true,
+                                  hint: 'Password',
+                                  onValidator: (value) {
+                                    return value.isEmpty
+                                        ? "enter password"
+                                        : null;
+                                  },
+                                ),
+                              ],
+                            ),
+                          )),
+                      const SizedBox(height: 40),
+                      RoundButton(
+                        title: 'Sign Up',
+                        loading: provider.loading,
+                        onPress: () {
+                          if (_formKey.currentState!.validate()) {
+                            provider.signUp(
+                              userNameController.text.toString(),
+                              emailController.text.toString(),
+                              passwordController.text.toString(),
+                            );
+                            // TODO: Implement login logic
+                            // emailController.clear();
+                            // passwordController.clear();
+                            // emailFocusNode.unfocus();
+                            // passwordFocusNode.unfocus();
+                          }
+                        },
+                      ),
+                      SizedBox(height: height * 0.03),
+                      InkWell(
+                        onTap: () {
+                          Navigator.pushNamed(context, RouteName.loginView);
+                        },
+                        child: Text.rich(
                           TextSpan(
-                            text: "Login",
-                            style: Theme.of(context)
-                                .textTheme
-                                .displayMedium!
-                                .copyWith(
-                                    fontSize: 15,
-                                    decoration: TextDecoration.underline),
-                          )
-                        ]),
+                              text: "Already have an account? ",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleMedium!
+                                  .copyWith(fontSize: 15),
+                              children: [
+                                TextSpan(
+                                  text: "Login",
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .displayMedium!
+                                      .copyWith(
+                                          fontSize: 15,
+                                          decoration: TextDecoration.underline),
+                                )
+                              ]),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-              ],
+                );
+              },
             ),
           ),
         ),
---
+++ lib/view_model/signup/signup_controller.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:tech_media/utils/Utils.dart';
+
+class SignupController with ChangeNotifier {
+  bool _loading = false;
+  bool get loading => _loading;
+
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  setLoading(bool value) {
+    _loading = value;
+    notifyListeners();
+  }
+
+  void signUp(String username, String email, String password) {
+    setLoading(true);
+    try {
+      auth
+          .createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      )
+          .then((value) {
+        Utils.toastMessage('User created successfully');
+        setLoading(false);
+      }).onError((error, stackTrace) {
+        Utils.toastMessage(error.toString());
+        setLoading(false);
+      });
+    } catch (e) {
+      Utils.toastMessage(e.toString());
+    }
+  }
+}
